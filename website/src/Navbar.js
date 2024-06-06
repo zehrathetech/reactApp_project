155775baacda5c6083ea523111ad2223e36f3708
zehrathetech <102900903+zehrathetech@users.noreply.github.com>
2024-06-06 08:02:57 +0000

    first commit

--- website/src/Navbar.js
+++ website/src/Navbar.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
+import './Navbar.css'
 function Navbar(){
     return(
 
-        <div className = "Design">
-            <div className="sub1">
-                <img src={logo} className="app-logo" alt="logo"/>
+        <div className = 'Design'>
+            <div className='sub1'>
+                <img src={logo} className="app-logo" alt="logo" height='50px' width='50px'/>
                 <p><i><b>React JS</b></i></p>
             </div>
 
-            <div className="sub2">
+            <div className='sub2'>
                 <input type="search" placeholder="SEARCH"></input>
             </div>
         </div>
